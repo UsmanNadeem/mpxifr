d85b344ea0212f5388df5d9c049d9195c675beb4
UsmanNadeem <usman@vt.edu>
2017-11-23 21:05:17 -0500

    fix makefiles

--- Test/test.c
+++ Test/test.c
@@ -2,22 +2,62 @@
 
 #include "stdio.h"
 #include "pthread.h"
+#include "inttypes.h"
+#include "string.h"
+#include <unistd.h>
+uint64_t x;
 
-int x;
-
+// void foo(void *data) {
+  // if (data != 0) printf("%s\n","Hello\n" );
+// }
 void *thread(void *data) {
-  for (int i = 0; i < 1000; i++) {
+  for (uint64_t i = 0; i < 1000000; i++) {
+    // usleep(1);
     x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // x += 1;
+    // foo(data);
+    // printf("%lu\n", x);
   }
+
+  return 0;
 }
 
 int main() {
   printf("Hello world\n");
-  x = 0;
-  pthread_t t1, t2;
+  x = 1;
+  pthread_t t1, t2, t3, t4;
   pthread_create(&t1, NULL, thread, NULL);
   pthread_create(&t2, NULL, thread, NULL);
+  // pthread_create(&t3, NULL, thread, NULL);
+  // pthread_create(&t4, NULL, thread, NULL);
+
+
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
-  printf("Final value of x: %d\n", x);
+  // pthread_join(t3, NULL);
+  // pthread_join(t4, NULL);
+  printf("Final value of x: %lu\n", x);
+
+  return 0;
 }
